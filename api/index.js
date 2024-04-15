066c38a6d037f327c24df5621e40a195505029fd
Satyam Kanojiya <kanojiyasatyam07@gmail.com>
2024-04-15 10:25:48 +0530

    added comment section to the post page

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
+import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
 const app = express();
 
@@ -28,6 +29,7 @@ app.listen(1000,()=>{
 app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
 app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
 
 
 // error handing middleware
