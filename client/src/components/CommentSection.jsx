1065f7b3367d624283fc30ea017bb02737a8ee0e
Satyam Kanojiya <kanojiyasatyam07@gmail.com>
2024-04-15 10:55:08 +0530

    added edit functionality to the comment component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -83,7 +83,13 @@ export default function CommentSection({ postId }) {
           console.log(error.message);
         }
       };
-
+      const handleEdit = async (comment, editedContent) => {
+        setComments(
+          comments.map((c) =>
+            c._id === comment._id ? { ...c, content: editedContent } : c
+          )
+        );
+      };
 
 
   return (
@@ -149,7 +155,7 @@ export default function CommentSection({ postId }) {
             </div>
           </div>
           {comments.map((comment) => (
-            <Comment key={comment._id} comment={comment}   onLike={handleLike} />
+            <Comment key={comment._id} comment={comment}   onLike={handleLike} onEdit={handleEdit} />
           ))}
         </>
       )}
